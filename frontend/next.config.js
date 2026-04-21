18034a9840464d2103b5008d4f9d12c3171ce397
BardalesE <bardaleselian82@gmail.com>
2026-04-20 22:00:10 -0500

    fix CSP imagenes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'res.cloudinary.com' },
-      { protocol: 'https', hostname: 'picsum.photos' },
-      { protocol: 'https', hostname: '**.railway.app' },
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "img-src 'self' * data: blob:;"
+          }
+        ]
+      }
     ]
   },
   env: {
